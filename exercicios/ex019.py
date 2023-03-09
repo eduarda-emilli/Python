e6631520bea7abf7587c16ef659e7998d37ec656
Eduarda <informaticaeduarda@gmail.com>
2023-03-08 21:19:39 -0300

    Create ex019.py

---
+++ exercicios/ex019.py
@@ -0,0 +1,26 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome escolhido.
+
+from random import choice
+from time import sleep
+import emoji
+
+a = input('Primeiro aluno(a): ')
+b = input('Segundo aluno(a): ')
+c = input('Terceiro aluno(a): ')
+d = input('Quarto aluno(a): ')
+
+sorteio = a, b, c, d
+escolha = choice(sorteio)
+
+
+sleep(0.5)
+
+print(emoji.emojize('O aluno escolhido foi... :question:', language='alias'))
+
+sleep(0.20)
+
+print(emoji.emojize(f'{escolha}! Meus parabéns! :tada:', language='alias'))
+
+
+
+

cfe1ef0f32f06e1befea0cb332a104f1c7726924
Eduarda <informaticaeduarda@gmail.com>
2023-05-18 19:37:07 -0300

    Delete ex030.py

--- exercicios/ex030.py
+++
@@ -1,2 +0,0 @@
-# Desenvolva um programa que pergfunte a distancia de uma viagem em Km. Calcule o preço da passagem, cobranco 0,50 centavos por KM para viagens
-# de até 200km a 0.45 centavos para viagens mais longas
